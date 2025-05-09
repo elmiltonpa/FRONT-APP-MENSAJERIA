f7c42847328aac77901da3ce84332363996997d4
milton <elmiltonibarra@gmail.com>
2025-05-09 18:11:09 -0300

    fix bug

--- src/services/socket.js
+++ src/services/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const URL = import.meta.env.VITE_API_URL;
+const URL = 'https://back-app-mensajeria.onrender.com/';
 
 const socket = io(URL, {
     withCredentials: true,
